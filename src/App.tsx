3b4259f29c6128f484c35244417f71f1c3f9a6ec
nhamonin <nazarii.hamonin@gmail.com>
2023-10-07 19:44:17 +0300

    Build app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import GlobalStyles from './styles/GlobalStyles';
+import AppLayout from './ui/AppLayout';
 import Dashboard from './pages/Dashboard';
 import Bookings from './pages/Bookings';
 import Account from './pages/Account';
@@ -16,14 +17,16 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="/dashboard" />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/bookings" element={<Bookings />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/cabins" element={<Cabins />} />
-          <Route path="/users" element={<Users />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="/dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/bookings" element={<Bookings />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/cabins" element={<Cabins />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/settings" element={<Settings />} />
+          </Route>
           <Route path="/login" element={<Login />} />
-          <Route path="/settings" element={<Settings />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
---
+++ src/ui/Sidebar.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledSidebar = styled.aside`
+  background-color: var(--color-grey-0);
+  border-right: 1px solid var(--color-grey-100);
+  padding: 3.2rem 2.4rem;
+  grid-row: 1 / -1;
+`;
+
+const Sidebar = () => {
+  return <StyledSidebar>Sidebar</StyledSidebar>;
+};
+
+export default Sidebar;
