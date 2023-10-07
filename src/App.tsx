8af4203593dc2c028b36e445b90f2e577ae5ffb6
nhamonin <nazarii.hamonin@gmail.com>
2023-10-07 18:54:00 +0300

    Setup up pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,32 @@
-import styled from 'styled-components';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import GlobalStyles from './styles/GlobalStyles';
-import Input from './ui/Input';
-import Button from './ui/Button';
-import Heading from './ui/Heading';
-import Row from './ui/Row';
-
-const StyledApp = styled.div`
-  padding: 20px;
-`;
+import Dashboard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import Account from './pages/Account';
+import Cabins from './pages/Cabins';
+import Users from './pages/Users';
+import Login from './pages/Login';
+import Settings from './pages/Settings';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">The Wild Oasis</Heading>
-
-            <div>
-              <Heading as="h2">Check in and out</Heading>
-              <Button onClick={() => alert('Check in!')}>Check in</Button>
-              <Button variation="secondary" size="small" onClick={() => alert('Check out!')}>
-                Check out
-              </Button>
-            </div>
-          </Row>
-
-          <Row>
-            <Heading as="h3">Form</Heading>
-            <form>
-              <Input placeholder="Number of guests" />
-              <Input placeholder="Number of guests" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/bookings" element={<Bookings />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/cabins" element={<Cabins />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
