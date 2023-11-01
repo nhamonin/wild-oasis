994f69dfc69d177b6fb50b4bfff7a1c8ac50a8cb
nhamonin <nazarii.hamonin@gmail.com>
2023-11-01 21:48:39 +0200

    Add prefetching for pagination to previous and next pages

--- src/features/bookings/hooks/useBookings.ts
+++ src/features/bookings/hooks/useBookings.ts
@@ -1,16 +1,24 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useSearchParams } from 'react-router-dom';
 
 import { getBookings } from '../../../services/apiBookings';
+import { PAGE_SIZE } from '../../../utils/constants';
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   const filterValue = searchParams.get('status');
-  const filter = !filterValue || filterValue === 'all' ? null : { field: 'status', value: filterValue };
+  const filter =
+    !filterValue || filterValue === 'all' ? null : { field: 'status', value: filterValue };
 
-  const [sortByValue, sortByMethod] = searchParams.get('sort_by')?.split('-') ?? ['start_date', 'desc'];
-  const sortBy = !sortByValue ? null : { field: sortByValue, method: sortByMethod as 'asc' | 'desc' };
+  const [sortByValue, sortByMethod] = searchParams.get('sort_by')?.split('-') ?? [
+    'start_date',
+    'desc',
+  ];
+  const sortBy = !sortByValue
+    ? null
+    : { field: sortByValue, method: sortByMethod as 'asc' | 'desc' };
 
   const page = Number(searchParams.get('page')) || 1;
 
@@ -26,6 +34,20 @@ export function useBookings() {
       },
     }
   );
+
+  const pageCount = Math.ceil(data?.count || 1 / PAGE_SIZE);
+
+  if (page < pageCount) {
+    queryClient.prefetchQuery(['bookings', filter, sortBy, page + 1], () =>
+      getBookings({ filter, sortBy, page: page + 1 })
+    );
+  }
+
+  if (page > 1) {
+    queryClient.prefetchQuery(['bookings', filter, sortBy, page - 1], () =>
+      getBookings({ filter, sortBy, page: page - 1 })
+    );
+  }
 
   return { isLoading, bookings: data?.bookings, count: data?.count, error };
 }
